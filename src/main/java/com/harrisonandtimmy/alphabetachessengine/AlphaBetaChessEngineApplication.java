a0c06a6f33652bc7b1bfb0fd1de17c9d86cf7def
Harrison Pham <pham.har@northeastern.edu>
2026-05-02 00:14:47 -0400

    set up project

---
+++ src/main/java/com/harrisonandtimmy/alphabetachessengine/AlphaBetaChessEngineApplication.java
@@ -0,0 +1,13 @@
+package com.harrisonandtimmy.alphabetachessengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBetaChessEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaBetaChessEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harrisonandtimmy/alphabetachessengine/AlphaBetaChessEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrisonandtimmy.alphabetachessengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaBetaChessEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
