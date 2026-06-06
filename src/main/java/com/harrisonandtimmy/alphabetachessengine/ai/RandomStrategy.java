3724960913961253a280f96210d7cac75e2e7a3c
Harrison Pham <pham.har@northeastern.edu>
2026-06-05 21:13:41 -0400

    add random strategy

--- src/main/java/com/harrisonandtimmy/alphabetachessengine/ai/RandomStrategy.java
+++ src/main/java/com/harrisonandtimmy/alphabetachessengine/ai/RandomStrategy.java
@@ -1,4 +1,22 @@
 package com.harrisonandtimmy.alphabetachessengine.ai;
 
-public class RandomStrategy {
+import com.harrisonandtimmy.alphabetachessengine.model.GameState;
+import com.harrisonandtimmy.alphabetachessengine.model.Move;
+
+import java.util.List;
+import java.util.Random;
+
+
+public class RandomStrategy implements AIStrategy {
+
+  private final Random random = new Random();
+
+  @Override
+  public Move getNextMove(GameState gameState) {
+    List<Move> legalMoves = gameState.getBoard().getLegalMoves(gameState.getCurrentTurn());
+
+    if (legalMoves.isEmpty()) return null;
+
+    return legalMoves.get(random.nextInt(legalMoves.size()));
+  }
 }
